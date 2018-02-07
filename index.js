4e7efff966eb05d3c3414ba5a4d9e216cc18b1f6
Jeffrey Damaso <jeffreydamaso340@gmail.com>
2018-02-07 01:20:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function theBeatlesPlay(musicians, instruments){
 function johnLennonFacts(facts){
   var array = [];
   let i = 0;
-  while (i < facts.length){
+  while (i < 4){
     
    array.push(`${facts[i]}!!!`);
     i++;
